de7c8418f8faa45673424bd67314bcced97468b2
Vaše jméno <betkakacirkova@gmail.com>
2024-06-13 14:38:02 +0200

    oprava

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import './App.css';
 import { Payment } from './assets/componenst/payment/payment';
 
 function App() {
-  const [pay, setPay] = useState<boolean>(false);
-  const handleClick = () => setPay(!pay);
   return (
     <>
       <h1 className="header-main">Programování karta</h1>
       <Payment />
-
-      
     </>
   );
 }
